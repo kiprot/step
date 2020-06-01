86c9e53d48ba7c49d1647f167857ef2a7e6bc043
Kenneth Kiprotich <kk3302@columbia.edu>
2020-06-01 15:14:18 -0400

    add a greeting feature to page

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -39,3 +39,9 @@ function toggleDetailsButton() {
   }
 }
 
+function getHelloMessage() {
+    fetch('/data').then(response => response.text()).then((message) => {
+        document.getElementById('message-container').innerText = message;
+    });
+}
+
